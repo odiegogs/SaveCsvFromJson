181130e76b554ce2fe74674bda9f08406415fa2d
Dell <Dell@DESKTOP-P1UER7B>
2022-04-12 15:58:33 -0300

    Add project files.

---
+++ SaveCsvFromJson/Controllers/CSVFromJsonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaveCsvFromJson.Models;
+
+namespace SaveCsvFromJson.Controllers
+{
+    public class CSVFromJsonController : Controller
+    {
+        public IActionResult Index()
+        {     
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GerarCSV(string txtjson)
+        {
+            DataSave ds = new DataSave();
+
+            //Checa se foi informado o texto no campo e se o formato é válido
+            if (ds.ValidaJson(txtjson) == false)
+            {
+                @ViewBag.jsonvalido = "É necessário informar um json";
+                return PartialView("~/views/home/index.cshtml");
+            }  
+        
+            //Metodo que gera o arquivo e atribui retorno
+            //a variável utilizada no ViewBag de retorno
+            ViewBag.Retorno = ds.SalvarCSV(txtjson);
+
+            return View("GerarCSV");
+        }
+
+    }
+}
---
+++ SaveCsvFromJson/Models/DataSave.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaveCsvFromJson.Models
+{
+    public class DataSave
+    {
+        private string JsonContent { get; set; }
+        public string Resultado { get; set; }
+        public bool JsonValido { get; set; }
+
+        public bool ValidaJson(string jsoncontent)
+        {
+            //Se há conteúdo
+            if (string.IsNullOrWhiteSpace(jsoncontent)) 
+                JsonValido = false;
+
+            //Verifica se inicia e termina com o padrão de json...
+            //Verificação simples se inicia no padrão do json, pois para fazer uma checagem mais completa,
+            //sem usar alguma biblioteca ou pacote teria que pesquisar pois não lembro de cabeça mais...
+            if ((jsoncontent.StartsWith("{") && jsoncontent.EndsWith("}")) || 
+                (jsoncontent.StartsWith("[") && jsoncontent.EndsWith("]"))) 
+            {                
+                JsonValido = true;
+            }
+            else
+            {
+                JsonValido = false;
+            }
+
+            return JsonValido;
+        }     
+
+        public string SalvarCSV(string jsoncontent)
+        {
+            if(ValidaJson(jsoncontent))
+            {
+                this.JsonContent = jsoncontent;
+                string[] valor = JsonContent.Replace("\"[{", "").Replace("}]\"", "").Replace("\"", "").Replace("{", "").Replace("}", "").Trim().Split(',');
+                var list = new List<KeyValuePair<string, string>>();
+
+                foreach (var item in valor)
+                {
+                    //Adiciona a chave e o value no list
+                    list.Add(new KeyValuePair<string, string>(item.Split(':')[0], item.Split(':')[1]));
+                }
+
+                //Join na string csv populando chave e valor para gerar CSV
+                string Csv = String.Join(Environment.NewLine, list.Select(x => $"{x.Key};{x.Value};"));
+                var path = @"wwwroot/Downloads/json.csv";
+
+                this.Resultado = Csv;
+
+                //Salvar CSV
+                File.WriteAllText(path, Csv);
+            }
+            
+            return Resultado;
+        }
+
+    }
+}
